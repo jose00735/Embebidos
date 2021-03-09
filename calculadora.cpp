1f6b47bd341ad59e0a4df100401f58676635d45c
Jose007 <josealejandroa99@gmail.com>
2021-03-09 17:18:55 -0500

    suma

--- calculadora.cpp
+++ calculadora.cpp
@@ -3,5 +3,21 @@
 using namespace std;
 
 int main(){
-	int n1 = 0, n2 = 0;
+	int r = 0,n1 = 0, n2 = 0;
+	int op = 0;
+	cout << "Ingrese a operacion deseada:" << endl << "1) suma" << endl << "2) Resta" << endl << ": ";
+	cin >> op;
+ 	cout << "Ingrese n1: ";
+	cin >> n1;
+       	cout << "Ingrese n2: ";
+	cin >> n2;	
+	if (op == 1){
+		r = n2 + n1;		
+	}
+	else
+	{
+		return 0;
+	}
+	cout << "El resultado: " << r << endl;
+	return 0;
 }
