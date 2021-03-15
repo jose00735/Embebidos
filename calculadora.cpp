941f04ac65abd9bb788dc4661a13ed3c8af38ce9
Jose007 <josealejandroa99@gmail.com>
2021-03-15 08:30:46 -0500

    division

--- calculadora.cpp
+++ calculadora.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(){
 	int r = 0,n1 = 0, n2 = 0;
 	int op = 0;
-	cout << "Ingrese a operacion deseada:" << endl << "1) suma" << endl << "2) Resta" << endl << "3) multiplicacion "<<": ";
+	cout << "Ingrese a operacion deseada:" << endl << "1) suma" << endl << "2) Resta" << endl << "3) multiplicacion "<< endl << "4) division" <<": ";
 	cin >> op;
  	cout << "Ingrese n1: ";
 	cin >> n1;
@@ -18,8 +18,12 @@ int main(){
 	{
 		r = n2 - n1;
 	}
-	else{
+	else if(op == 3){
 		r = n2*n1;
+	}
+	else 
+	{
+		r = n2/n1;
 	}
 	cout << "El resultado: " << r << endl;
 	return 0;
